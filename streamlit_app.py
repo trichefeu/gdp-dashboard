027831dad09aa302a0312bfcee135f422fe5da7a
trichefeu <trichefeu@euronext.com>
2024-08-06 09:33:12 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import math
 from pathlib import Path
 
+
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
     page_title='Streamlit test dashboard',
